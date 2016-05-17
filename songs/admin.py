d049c89a9da8ed4c78cc5f7b620f6eeb1529eb4e
JMH <jon@simplyagree.net>
2016-05-17 16:39:06 -0500

    Registering models in admin

--- songs/admin.py
+++ songs/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Program, Episode, Artist, Song, Interlude
+
+
+admin.site.register(Program)
+admin.site.register(Episode)
+admin.site.register(Artist)
+admin.site.register(Song)
+admin.site.register(Interlude)
