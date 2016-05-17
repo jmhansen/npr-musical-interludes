772ae28b9ffe9bcf0142122343f28408dbf1005e
JMH <jon@simplyagree.net>
2016-05-17 15:32:11 -0500

    Configuring for deployment to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn npr_interludes.wsgi --log-file -
--- npr_interludes/settings/base.py
+++ npr_interludes/settings/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
@@ -23,6 +24,17 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+
+# Parse database configuration from $DATABASE_URL
+DATABASES = {'default': dj_database_url.config()}
+DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 # Application definition
